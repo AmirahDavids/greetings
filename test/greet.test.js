0b82040e2fcdf4265319f1bf6b81787e0fe4056f
Amirah <amirahdavids2503@gmail.com>
2020-05-20 20:20:30 +0200

    created tests for the functions

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,24 +1,55 @@
 
-describe("The greet function ", function () {
+describe("The greetUser function", function () {
     it("should greet Amirah correctly", function () {
-        assert.equal("Hello, Amirah");
+        var greetFactoryOne = GreetFactory()
+        assert.equal("Hello, Amirah", greetFactoryOne.greetUser("Amirah", "english"));
     });
     it("should greet Susan correctly", function () {
-        assert.equal("Hello, Susan");
+        var greetFactoryOne = GreetFactory()
+        assert.equal("Hallo, Susan", greetFactoryOne.greetUser("Susan", "afrikaans"));
+    });
+    it("should greet Mecayle correctly", function () {
+        var greetFactoryOne = GreetFactory()
+        assert.equal("Bonjour, Mecayle", greetFactoryOne.greetUser("Mecayle", "french"));
     });
 });
-describe("The  function ", function () {
-    it("", function () {
-        assert.equal(", ");
-    });
-    it("", function () {
-        assert.equal(", ");
-    });
-});
-describe("The validateInput function ", function () {
-    it("should remove all numbers and special characters from textbox input", function () {
-        var instance = GreetFactory();
 
-        assert.equal("Yaanie",instance.validateInput("1234yaanie123456@$#@$@#%@%$#$"));
+describe("The getGreetCount function", function () {
+    it("should return the total number of users greeted", function () {
+        var greetFactoryOne = GreetFactory();
+        assert.equal(0,greetFactoryOne.getGreetCount());
+    });
+    it("should return the total number of users greeted", function () {
+        var greetFactoryOne = GreetFactory();
+        greetFactoryOne.greetUser("Mujahid","english")
+        assert.equal(1,greetFactoryOne.getGreetCount());
+    });
+});
+
+describe("The getAllUsers function", function () {
+    it("should return the object of all users greeted", function () {
+        var greetFactoryOne = GreetFactory();
+        greetFactoryOne.greetUser("Daiyaan","french");
+        greetFactoryOne.greetUser("Mujahid","english");
+        assert.deepEqual({"Daiyaan":0,"Mujahid":0},greetFactoryOne.getAllUsers());
+    });
+    it("should return the object of all users greeted", function () {
+        var greetFactoryOne = GreetFactory();
+        greetFactoryOne.greetUser("Mujahid","english");
+        assert.deepEqual({"Mujahid":0},greetFactoryOne.getAllUsers());
+    });
+    it("should return the total number of users greeted", function () {
+        var greetFactoryOne = GreetFactory();
+        greetFactoryOne.greetUser("Mujahid","english")
+        assert.equal(1,greetFactoryOne.getGreetCount());
+    });
+});
+
+describe("The getNameFromInput function", function () {
+    it("should return a name without numbers or special characters", function () {
+        var greetFactoryOne = GreetFactory()
+        assert.equal("Amirah", greetFactoryOne.getNameFromInput("34AM@%$#ir55//4a><h"));
+        assert.equal("Yaanie",greetFactoryOne.getNameFromInput("1234yaanie123456@$#@$@#%@%$#$"));
+        assert.equal("",greetFactoryOne.getNameFromInput(""));
     });
 });
