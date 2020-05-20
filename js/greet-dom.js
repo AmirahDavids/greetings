9ca507dbfa1117a67bbbe8e3275f9e3aea5b1720
Amirah <amirahdavids2503@gmail.com>
2020-05-20 20:16:25 +0200

    using factory function inside of the Dom

--- js/greet-dom.js
+++ js/greet-dom.js
@@ -1,22 +1,38 @@
-window.addEventListener("DOMContentLoaded",function(){
+window.addEventListener("DOMContentLoaded", function () {
+
+    // these are refernces to the elements on the screen
+    var inputBox = document.getElementById("inputElement")
+    var greetBtn = document.getElementById("greetButton")
+    var message = document.getElementById("msg")
+    var total = document.getElementById("counter")
+
+    // this is getting the users from local storage
+    var stored = JSON.parse(localStorage['greetedUsers']);
     
+    //this the instance of the greet factory with stored--
+    var greetFactory = GreetFactory(stored);
 
+    window.addEventListener("load", function () {
+        total.innerHTML = greetFactory.getGreetCount();
+    });
 
+    // this is the greet buttons event handler 
+    greetBtn.addEventListener("click", function () {
+        var input = inputBox.value;
+        var radioBtn = document.querySelector("input[name='selector']:checked");
+        if (radioBtn) {
+            var language = radioBtn.value;
+            var name = greetFactory.getNameFromInput(input)
+            if (name !== "") {
+                message.innerHTML = greetFactory.greetUser(name, language)
+                total.innerHTML = greetFactory.getGreetCount();
+                localStorage['greetedUsers'] = JSON.stringify(greetFactory.getAllUsers());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+            } else {
+                message.innerHTML = `no name entered.`
+            }
+        } else {
+            message.innerHTML = `no language selected.`
+        }
+    });
 });
